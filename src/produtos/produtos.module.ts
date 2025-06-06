153e36efc0c53c1e8ae7f1687819563bb027e59d
arthur.vitorino <arthurvitorino.dev@gmail.com>
2025-06-06 11:07:14 -0300

    correção de erro na importação do módulo de produtos

--- src/produtos/produtos.module.ts
+++ src/produtos/produtos.module.ts
@@ -5,9 +5,11 @@ import { Produto } from './entities/produto.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Categoria } from 'src/categorias/entities/categoria.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Produto, Categoria]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Produto, Categoria, User]), AuthModule, UserModule],
   controllers: [ProdutosController],
   providers: [ProdutosService],
 })
